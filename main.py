b2c89332f40c7161b62d8038e08c13017cae12c9
Johannes Larsson <johannes.larsson@protonmail.com>
2017-11-10 21:16:00 +0100

    stuff works as it should

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import praw
 import sqlite3
 import json
+from praw.models import Message
 from pprint import pprint
+import messageMaker
 
 with open('login.json') as login_info:
     login = json.load(login_info)
@@ -18,12 +20,43 @@ reddit = praw.Reddit(client_id=login['clientId'],
 print('logged in as ' + str(reddit.user.me()))
 
 
-for message in reddit.inbox.messages():
-    if message.new:
+# check new messages
+for message in reddit.inbox.unread(limit=None):
+    if isinstance(message, Message):
         print('new message from ' + message.author.name + ':')
         print(message.subject)
         print(message.body)
-        if 'code swap' in message.body.lower() + message.subject.lower():
+        if 'no' in message.body.lower() + message.subject.lower():
             message.mark_read()
-            cursor.execute('insert into users (name, recieve_date) values (:name, (select date()))',{'name':message.author.name})
+            cursor.execute('delete from users where name = ?', (message.author.name,))
             db.commit()
+            message.author.message('Code swap', messageMaker.declined())
+        elif 'code swap' in message.body.lower() + message.subject.lower():
+            message.mark_read()
+            message.author.message('Code swap', messageMaker.firstContact())
+            cursor.execute('insert into users (name, recieve_date) \
+            values (:name, (select date()))',
+            {'name':message.author.name})
+            db.commit()
+
+# pair people up
+cursor.execute('select name, id from users where partner is null\
+ order by recieve_date asc')
+while True:
+    user1 = cursor.fetchone()
+    user2 = cursor.fetchone()
+    if user1 == None or user2 == None:
+        break
+    else:
+        r1 = reddit.redditor(user1[0])
+        r2 = reddit.redditor(user1[0])
+
+        print('new pair: ' + r1.name + ' and ' + r2.name)
+
+        r2.message('Code swap', messageMaker.pair(r2.name))
+        r2.message('Code swap', messageMaker.pair(r1.name))
+
+        cursor.executemany('update users\
+         set partner = ?, partnered_date = (select date()) where id = ?',
+         [user1, user2])
+        db.commit()
---
+++ messageMaker.py
@@ -0,0 +1,16 @@
+def firstContact():
+    return 'Hello!\n\n\
+I will message you back when I find you a code swap partner. \n\n\
+^If ^you ^don\'t ^want ^that, ^reply ^to ^this ^message ^with ^"no".\
+\n\n//the code swap bot'
+
+def declined():
+    return 'Ok. You are no longer on the list.\
+\n\n//the code swap bot'
+
+def pair(username):
+    return 'Hello! \n\n\
+Looks like /u/' + username + ' \
+wants to swap codes with you.\n\n\
+^Please ^keep ^in ^mind ^that ^I\'m ^just ^a ^bot. ^I ^don\'t ^know ^if ^they ^might ^scam ^you. ^That ^judgement ^is ^for ^you ^to ^make, ^with ^your ^amazing ^human ^brain.\
+\n\n//the code swap bot'
